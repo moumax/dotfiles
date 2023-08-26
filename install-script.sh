202f6989929e29636e60550b25af233f1abdf977
Marc LANTOL <marc.lantol@gmail.com>
2023-08-26 11:24:26 +0200

    Feat : new Install script

---
+++ install-script.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+
+set -e pipefail
+
+Os="$(uname -s)"
+
+if [ ! "$Os" = "Linux" ]; then 
+	printf "\n"
+	printf "  ================================================\n"
+	printf "         Script uniquement compatible Linux       \n"
+	printf "  ================================================\n"
+	exit 1
+fi
+
+if ! which node > /dev/null; then
+		printf "\n"
+		printf "  ================================================\n"
+		printf "      Vous devez installer NodeJs en premier      \n"
+		printf "  ================================================\n"
+		exit 1
+fi
+
+printf "\n"
+printf "  =========================================\n"
+printf "    Script d'installation Web dev Ubuntu   \n"
+printf "  =========================================\n"
+printf "\n"
+printf "  =========================================\n"
+printf "           Sélectionnez un choix           \n"
+printf "\n"
+printf "   	1 -- Update et Upgrade du système      \n"
+printf "   	q -- Quitter le script                 \n"
+
+read -p "Votre choix ?" choice
+
+if [ "$choice" = "q" ]; then
+	printf "Bye"
+	exit 1
+fi
