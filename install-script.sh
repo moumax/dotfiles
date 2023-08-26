8c61a97a5a40766ecf94fcec3c1358667a34a2bf
Marc LANTOL <marc.lantol@gmail.com>
2023-08-26 16:13:33 +0200

    Add starship install option

--- install-script.sh
+++ install-script.sh
@@ -30,10 +30,12 @@ printf "           Sélectionnez un choix           \n"
 printf "\n"
 printf "   	1 -- Update et Upgrade du système      \n"
 printf "   	2 -- Création de l'archi dossier       \n"
-printf "   	3 -- Intallation de Alacritty          \n"
+printf "   	3 -- Intallation de la font hack       \n"
+printf "   	4 -- Intallation de Alacritty          \n"
+printf "   	5 -- Intallation de Starship           \n"
 printf "   	q -- Quitter le script                 \n"
 
-read -p "Votre choix ?" choice
+read -p "Votre choix ? " choice
 
 if [ "$choice" = "1" ]; then
 	cd ~
@@ -55,6 +57,18 @@ if [ "$choice" = "2" ]; then
 fi
 
 if [ "$choice" = "3" ]; then
+	wget -P ~/downloads https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip && \
+	cd ~/.local/share && \
+	mkdir fonts && \
+	cd fonts && \
+	mv ~/downloads/Hack.zip . && \
+	unzip Hack.zip
+	printf "  =========================================\n"
+	printf "       Fin de l'installation de la font    \n"
+	printf "  =========================================\n"
+fi
+
+if [ "$choice" = "4" ]; then
 	sudo apt update && sudo apt upgrade
 	sudo add-apt-repository ppa:aslatter/ppa -y
 	sudo apt install alacritty
@@ -62,6 +76,17 @@ if [ "$choice" = "3" ]; then
 	stow -t ~/.config/alacritty alacritty
 	printf "  =========================================\n"
 	printf "      Fin de l'installation d'Alacritty    \n"
+	printf "  =========================================\n"
+fi
+
+if [ "$choice" = "5" ]; then
+	cd ~/ && \
+	curl -sS https://starship.rs/install.sh | sh
+	mkdir -p ~/.config/starship
+	cd ~/dev/dotfiles && \
+	stow -t ~/.config/starship starship
+	printf "  =========================================\n"
+	printf "      Fin de l'installation de starship    \n"
 	printf "  =========================================\n"
 fi
 
